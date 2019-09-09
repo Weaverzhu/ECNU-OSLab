94e214ee34b685abc72dbe16facf267072262cef
Weaverzhu <weaver_zhu@outlook.com>
2019-09-09 11:29:13 +0800

    190909 11:24

--- lab1/0a/s2.sh
+++ lab1/0a/s2.sh
@@ -1,4 +1,4 @@
 rm -f output
-find "/bin" -iname "b*" -type f | xargs ls -l| awk '{print $9" "$3" "$1}' >> output
+find "/bin" -iname "b*" -type f | xargs ls -l | awk '{print $9" "$3" "$1}' >> output
 sed -i 's:/bin/::g' output
 chmod 644 output
