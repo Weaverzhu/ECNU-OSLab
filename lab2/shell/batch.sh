5bf4e5539cab7764e80da014a130f8ca7b0b0558
Weaverzhu <weaver_zhu@outlook.com>
2019-09-30 16:39:16 +0800

    fix: should read line instead of read 512 bytes

--- lab2/shell/batch.sh
+++ lab2/shell/batch.sh
@@ -1,1 +1,6 @@
-ls
+ uname
+  uname
+/bin/ls  /bin/
+/bin/ls   /bin/
+/usr/bin/which cp 
+/usr/bin/which cp  
--- lab2/shell/mysh.c
+++ lab2/shell/mysh.c
@@ -17,13 +17,14 @@ int main(int argc, char const *argv[])
 
     dbg("started");
 
-    int SOURCE_FD = STDOUT_FILENO;
+    // int SOURCE_FD = STDOUT_FILENO;
+    FILE *f =stdin;
 
     if (argc > 1) {
         dbg("batch mode on");
         BATCH_MODE = 1;
-        SOURCE_FD = open(argv[1], O_RDONLY);
-        if (SOURCE_FD < 0) {
+        f= fopen(argv[1], "r");
+        if (f == NULL) {
             REPORT_ERR;
             exit(-1);
         }
@@ -31,7 +32,7 @@ int main(int argc, char const *argv[])
 
     dup2(STDIN_FILENO, ORIGIN_STDIN_FILENO);
     dup2(STDOUT_FILENO, ORIGIN_STDOUT_FILENO); // make backup for stdin, stdout
-
+    
     #ifdef DEBUG
     printf("%d %d\n", BATCH_MODE, SOURCE_FD);
 
@@ -42,15 +43,15 @@ int main(int argc, char const *argv[])
         static char cmdline[SIZE];
         PRINT_HEADER; // write header
         memset(cmdline, 0, sizeof cmdline); // clear the str
-        int ret = read(SOURCE_FD, cmdline, SIZE); // read 512 bytes
-        if (ret == 0) break;
-        if (ret == -1) { // go wrong
-            REPORT_ERR;
-            break;
-        } else if (ret == 1) { // empty file, only a \n left
+        // int ret = read(SOURCE_FD, cmdline, SIZE); // read 512 bytes
+        char *rets = fgets(cmdline, SIZE, f);
+        if (rets == NULL) break;
+        else if (strlen(cmdline) == 1) { // empty file, only a \n left
             dbg("empty command");
             continue;
         }
+        int ret = 0;
+        // printf("%s\n", cmdline);
         CmdList *head = parseLine(cmdline); // a bunch of cmds
 
         #ifdef DEBUG
