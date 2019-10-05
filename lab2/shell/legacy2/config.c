2a6632929e597ded71057d21b4c0f2717edcf68b
Weaverzhu <weaver_zhu@outlook.com>
2019-10-05 15:14:44 +0800

    remove legacy

--- lab2/shell/legacy2/config.c
+++
@@ -1,9 +0,0 @@
-#include "config.h"
-#include <string.h> 
-
-
-
-const char error_message[30] = "An error has occurred\n";
-const char HEADER[] = "mysh> ";
-
-
--- lab2/shell/legacy2/mysh.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MYSH_H_
-#define _MYSH_H_
-
-#include "cmd.h"
-
-extern CmdList *bghead;
-
-#endif
