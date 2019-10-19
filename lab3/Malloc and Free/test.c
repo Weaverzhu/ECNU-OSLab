7ad7a95bd0f9a1c7bc96f074f0bef5e11c6e52ce
Weaverzhu <weaver_zhu@outlook.com>
2019-10-19 09:21:41 +0800

    func definition fix & insertbeforenode func

--- lab3/Malloc and Free/test.c
+++ lab3/Malloc and Free/test.c
@@ -15,13 +15,38 @@ int m_error;
 
 typedef struct __node_t {
     int size;
-    struct __node_t *next;
+    void *prev, *next;
 } node_t;
+
 
 
 typedef struct __header_t {
     int size, magic;
+    void *prev, *next;
 } header_t;
+
+void setnode(node_t *t, int size, void *prev, void *next);
+void setheader(header_t *t, int size, void *prev, void *next);
+void *test_alloc(int size);
+node_t *insertBeforeNode(int size, node_t *head, node_t *b);
+
+
+//////////////////////////////////////////////////////////////
+
+void setnode(node_t *t, int size, void *prev, void *next) {
+    t->size = size;
+    t->prev = prev;
+    t->next = next;
+}
+
+void setheader(header_t *t, int size, void *prev, void *next) {
+    t->size = size;
+    t->magic = MAGIC;
+    t->prev = prev;
+    t->next = next;
+}
+
+
 
 node_t *head;
 void *ptr;
@@ -51,8 +76,8 @@ int mem_init(int size_of_region)
 
     // initialize the link list
     head = ptr;
-    head->next = NULL;
-    head->size = size_of_region - sizeof(node_t);
+    setnode(head, size_of_region - sizeof(node_t), NULL, NULL);
+
 
     // close the device (don't worry, mapping should be unaffected)
     close(fd);
@@ -67,14 +92,25 @@ void *test_alloc(int size) {
 node_t *insertBeforeNode(int size, node_t *head, node_t *b) {
     int totsize = size + sizeof(header_t);
     if (b == head) {
-        node_t *newhead = head + totsize;
-        newhead->size = head->size - totsize;
-        newhead->next = head->next;
-        memset(head, 0, totsize); // clear the memory
-        return 
-    } else {
+        // set the new head node
+        node_t *newhead = (void*)head + totsize;
+        setnode(newhead, head->size - totsize, head->prev, head->next);
 
+        // clear the memory
+        memset(head, 0, totsize); 
+
+        // return new head node
+        return newhead;
+    } else {
+        node_t *newnode = (void*)b + totsize;
+        setnode(newnode, b->size - totsize, b->prev, b->next);
+
+        // clear the memory
+        memset(b, 0, totsize);
+        return head;
     }
+
+    return head;
 }
 
 void *mem_alloc(int size, int style) {
